2fed91d37f64574df3be4be692deefdf58f8b6c7
Hung <daminhnguyenhung@gmail.com>
2023-02-15 18:39:47 +0700

    done header

---
+++ src/assets/svg/index.js
@@ -0,0 +1,9 @@
+const svg = {
+    facebook: require('./facebook.svg').default,
+    instagram: require('./instagram.svg').default,
+    tiktok: require('./tiktok.svg').default,
+    twitter: require('./twitter.svg').default,
+    search: require('./search.svg').default,
+};
+
+export default svg;
--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import svg from '~/assets/svg';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <div className={cx('wrapper')}>header</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('top-left')}>
+                <img className={cx('icon')} src={svg.facebook} alt="facebook icon" />
+                <img className={cx('icon')} src={svg.tiktok} alt="tiktok icon" />
+                <img className={cx('icon')} src={svg.twitter} alt="twitter icon" />
+                <img className={cx('icon')} src={svg.instagram} alt="instagram icon" />
+            </div>
+            <div className={cx('top-right')}>
+                <div className={cx('nav')}>
+                    <Link>
+                        <div className={cx('nav-item')}>Home</div>
+                    </Link>
+                    <Link>
+                        <div className={cx('nav-item')}>About</div>
+                    </Link>
+                    <Link>
+                        <div className={cx('nav-item')}>Contact</div>
+                    </Link>
+                    <Link>
+                        <div className={cx('nav-item')}>Login</div>
+                    </Link>
+                    <Link>
+                        <div className={cx('nav-item')}>Register</div>
+                    </Link>
+                </div>
+                <div className={cx('search')}>
+                    <img src={svg.search} alt="search icon" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
